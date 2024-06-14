aba1e3bae7703ae29fdf777de5369205f8e8f6c9
DaRam810 <148483333+DaRam810@users.noreply.github.com>
2024-06-14 09:06:30 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var popup = document.getElementById("popup");
+var popupShown = localStorage.getItem("popupShown");
+
+function closePopup() {
+  popup.style.display = "none";
+  localStorage.setItem("popupShown", "true");
+}
+
+if (popupShown === null || popupShown === "false") {
+  popup.style.display = "block";
+}
